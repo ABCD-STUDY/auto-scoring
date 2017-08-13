430d1a7adb4ba2b906db298d4c32d9b71f0bb7bd
Hauke Bartsch <HaukeBartsch@gmail.com>
2017-08-13 16:18:55 -0700

    runner can stop processing and restart (in preparation of debugging)

--- runner/nodes/redcap_get.js
+++ runner/nodes/redcap_get.js
@@ -1,14 +1,11 @@
 //
 // do a redcap_get and write the requested values into the output
 //
-var async = require("async");
+var async   = require("async");
 var request = require('request');
-var fs = require('fs');
-var path = require('path');
+var fs      = require('fs');
+var path    = require('path');
 
-
-//var participants = null;
-//var numCalls = 0; // can we call now many times we call REDCap? If this value is 0 we are done - or we did not start yet
 
 var RedcapGet = function (state) {
     this._state = state; // this contains the list of variables we need to query from REDCap, ask for them initially instead of in the worker
@@ -17,10 +14,77 @@ var RedcapGet = function (state) {
     this._nextEpoch = false;
     this._currentEpoch = 0;
     this._waitingForData = true;
+    this._startedGetAllData = false;
 
+    // We should not automatically pull data in, this will prevent a startup of this node from a stored state file
+    // We should react to the first readyForEpoch instead to start the pull of data into the system.
+    //this.getAllData();
+};
+
+// gets a notification if a new epoch started - not used for this node
+RedcapGet.prototype.startEpoch = function () { }
+
+RedcapGet.prototype.endEpoch = function () {
+    this._participants.shift(); // get the next subject      
+}
+
+RedcapGet.prototype.readyForEpoch = function () {
+    if (!this._startedGetAllData) {
+	this._startedGetAllData = true;
+	this.getAllData();
+    }
+    return !this._waitingForData;
+}
+
+RedcapGet.prototype.doneDone = function () {
+    if (this._numCalls > 0 || this._participants.length > 0) {
+        return false; // still work to do in the next epoch
+    }
+    return true; // nothing more to do
+}
+
+// inputs and state are read only, outputs can be written
+RedcapGet.prototype.work = function (inputs, outputs, state) {
+    if (this._participants.length < 1) {
+        console.log("could not get any participants from REDCap, nothing to do here...");
+        return;
+    }
+
+    // this will produce many new values, we should act like a generator (lazy evaluation) and return them one at a time
+    // if there are no more values we should tell upstairs
+
+    // pull one of the participants out and get the requested values for that participant
+    var entry = this._participants[0];
+    if (entry === undefined) {
+        // is there another entry?
+        console.log("got entry as undefined");
+    }
+
+    var pGUID = entry['id_redcap'];
+    var redcap_event_name = entry['redcap_event_name'];
+
+
+    outputs['id_redcap'] = pGUID;
+    outputs['redcap_event_name'] = redcap_event_name;
+    for (var i = 0; i < state.length; i++) {
+        if (typeof state[i]['value'] === 'undefined')
+            continue;
+        if (typeof entry[ state[i]['value'] ] === 'undefined' ) {
+            //console.log("Error: tried to get variable \"" + state[i]['value'] + "\" from REDCap but could not get it.");
+            continue;
+        }
+        if ( state[i]['value'] === 'id_redcap' || state[i]['value'] === 'redcap_event_name' )
+            continue; // don't overwrite
+        // find out if we have to look for a checkbox, in that case ask for the variable instead
+        var val = entry [ state[i]['value'] ];
+        outputs[ state[i]['value'] ] = val;
+    }
+};
+
+RedcapGet.prototype.getAllData = function() {
     // get the configuration for this node
-    //var tokens = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../../code/php/tokens.json'), 'utf8'));
-    var tokens = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../tokens.json'), 'utf8'));
+    var tokens = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../../code/php/tokens.json'), 'utf8'));
+    //var tokens = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../tokens.json'), 'utf8'));
 
     // we are called here the first time, work is called every time and done tells us if we done
     var queue = async.queue(function (st, callback) {
@@ -128,73 +192,6 @@ var RedcapGet = function (state) {
         })(site, this));
         this._numCalls = this._numCalls + 1;
     }
-};
-
-// gets a notification if a new epoch started - not used for this node
-RedcapGet.prototype.startEpoch = function () { }
-
-RedcapGet.prototype.endEpoch = function () {
-    this._participants.shift(); // get the next subject      
 }
-
-RedcapGet.prototype.readyForEpoch = function () {
-    return !this._waitingForData;
-}
-
-RedcapGet.prototype.doneDone = function () {
-    if (this._numCalls > 0 || this._participants.length > 0) {
-        return false; // still work to do in the next epoch
-    }
-    return true; // nothing more to do
-}
-
-// inputs and state are read only, outputs can be written
-RedcapGet.prototype.work = function (inputs, outputs, state) {
-    if (this._participants.length < 1) {
-        console.log("could not get any participants from REDCap, nothing to do here...");
-        return;
-    }
-
-    // this will produce many new values, we should act like a generator (lazy evaluation) and return them one at a time
-    // if there are no more values we should tell upstairs
-
-    // pull one of the participants out and get the requested values for that participant
-    var entry = this._participants[0];
-    if (entry === undefined) {
-        // is there another entry?
-        console.log("got entry as undefined");
-    }
-
-
- /*   if (this._nextEpoch) {
-        this._participants.shift(); // get the next subject
-        if (this._participants.length < 1) {
-            console.log("could not get any participants from REDCap, nothing to do here...");
-            return;
-        }
-        entry = this._participants[0];
-        this._nextEpoch = false;
-        //console.log("pull a new participant: " + entry['id_redcap'] + ", " + entry['redcap_event_name'] + " (" + this._participants.length + ")");
-    } */
-    var pGUID = entry['id_redcap'];
-    var redcap_event_name = entry['redcap_event_name'];
-
-
-    outputs['id_redcap'] = pGUID;
-    outputs['redcap_event_name'] = redcap_event_name;
-    for (var i = 0; i < state.length; i++) {
-        if (typeof state[i]['value'] === 'undefined')
-            continue;
-        if (typeof entry[ state[i]['value'] ] === 'undefined' ) {
-            //console.log("Error: tried to get variable \"" + state[i]['value'] + "\" from REDCap but could not get it.");
-            continue;
-        }
-        if ( state[i]['value'] === 'id_redcap' || state[i]['value'] === 'redcap_event_name' )
-            continue; // don't overwrite
-        // find out if we have to look for a checkbox, in that case ask for the variable instead
-        var val = entry [ state[i]['value'] ];
-        outputs[ state[i]['value'] ] = val;
-    }
-};
 
 module.exports = RedcapGet;
--- runner/runner.js
+++ runner/runner.js
@@ -1,38 +1,52 @@
 #!/usr/bin/env nodejs
-
 //#!/usr/bin/env node
+
 
 // run this with:
 //   ./runner.js run -i ../viewer/recipes/FirstTest.json
+
+//
+// for testing purposes you can write the node values out to test.json with (streaming json):
+//   ./runner.js run -h history.txt -i ../viewer/recipes/cna.json
+
+//
+// for testing the debugging do:
+//   ./runner.js run -d /tmp/debug.json -s 10 ../viewer/recipes/ssrs.json
 
 'use strict';
 
 const program = require('commander');
 var request = require('request');
 var fs = require('fs');
+//var zlib = require('zlib');
 var path = require('path');
 
-var Not         = require("./nodes/not.js")
-var Equal       = require("./nodes/equal.js")
-var RedcapGet   = require("./nodes/redcap_get.js")
-var RedcapPut   = require("./nodes/redcap_put.js")
-var Mean        = require("./nodes/mean.js")
-var Median      = require("./nodes/median.js")
-var Sum         = require("./nodes/sum.js")
-var AllNotEmpty = require("./nodes/all-not-empty.js")
-var NotThisValue = require("./nodes/not-this-value.js")
-var ConsoleOut  = require("./nodes/console-out.js")
-var IfElse      = require("./nodes/if-else.js")
+var Not            = require("./nodes/not.js")
+var Equal          = require("./nodes/equal.js")
+var RedcapGet      = require("./nodes/redcap_get.js")
+var RedcapPut      = require("./nodes/redcap_put.js")
+var Mean           = require("./nodes/mean.js")
+var Median         = require("./nodes/median.js")
+var Sum            = require("./nodes/sum.js")
+var AllNotEmpty    = require("./nodes/all-not-empty.js")
+var NotThisValue   = require("./nodes/not-this-value.js")
+var ConsoleOut     = require("./nodes/console-out.js")
+var IfElse         = require("./nodes/if-else.js")
 var ConstantNumber = require("./nodes/constant-number.js")
-var ConstantText = require("./nodes/constant-text.js")
-var SmallerEqual = require("./nodes/smallerequal.js")
-var GreaterEqual = require("./nodes/greaterequal.js")
-var Maximum      = require("./nodes/maximum.js")
-var Minimum      = require("./nodes/minimum.js")
-var Filter       = require("./nodes/filter.js")
-var TScorer      = require("./nodes/t-score.js")
+var ConstantText   = require("./nodes/constant-text.js")
+var SmallerEqual   = require("./nodes/smallerequal.js")
+var GreaterEqual   = require("./nodes/greaterequal.js")
+var Maximum        = require("./nodes/maximum.js")
+var Minimum        = require("./nodes/minimum.js")
+var Filter         = require("./nodes/filter.js")
+var TScore         = require("./nodes/t-score.js")
 
 var exportFileName = "";
+// if we are asked to debug we will store the evaluation of the graph into this file
+// the viewer should be able to read this later and replay
+var historyFile = "";
+var debugFile   = "";
+var numSteps    = 0;
 
 // return the values for a node from the incoming connections (each incoming connections source nodes internal state value)
 function getInputValues(n, recipe) {
@@ -45,7 +59,8 @@ function getInputValues(n, recipe) {
         var thisinputName = thisinput['name'];
         var id = thisinput['id'];
         for (var i = 0; i < connections.length; i++) {   	// find this inputs connections
-            if (connections[i]['target'].indexOf(id) !== -1) {  // found one connection that leads to this node (inputs have to be singeltons? Or can we have more than one input and we pick a random value?)
+            if (connections[i]['target'].indexOf(id) !== -1) {
+		// found one connection that leads to this node (inputs have to be singeltons? Or can we have more than one input and we pick a random value?)
                 // what is the node on the other end of the connection? This node is the target.
                 var targetNodeID = connections[i]['source'];
                 var targetNode = null;
@@ -53,7 +68,6 @@ function getInputValues(n, recipe) {
                 for (var j = 0; j < nodes.length; j++) {
                     for (var k = 0; k < nodes[j]['outputs'].length; k++) {
                         if (targetNodeID.indexOf(nodes[j]['outputs'][k]['id']) !== -1) {
-                            // found the target node
                             targetNode = nodes[j];
                             outputPort = k;
                             break;
@@ -67,15 +81,16 @@ function getInputValues(n, recipe) {
                     // ok, this is the node on the other end. What is the state value for this output port?
                     var value = undefined;
                     if (typeof targetNode['outputs'] !== 'undefined') {
-                        // value = targetNode['state'][outputPort]['value'];  // this is the internal value, not the calculated one
                         if (typeof targetNode['outputs'][outputPort]['value'] !== 'undefined')
                             value = targetNode['outputs'][outputPort]['value'];
                     }
 
                     // add to inputs
                     // for undefined this would indicate that a value was expected - but could not be set by the recipe at this point in time
-                    if (typeof inputs[thisinputName] === 'undefined' || inputs[thisinputName] === undefined) // if we have more than one connection to this node we should use the last one that has a value not undefined
+                    if (typeof inputs[thisinputName] === 'undefined' || inputs[thisinputName] === undefined) {
+			// if we have more than one connection to this node we should use the last one that has a value not undefined
                         inputs[thisinputName] = value;
+		    }
                 }
             }
         }
@@ -90,7 +105,8 @@ function getEnabledValue(n, recipe) {
 
     var id = n['enabledisable-id'];
     for (var i = 0; i < connections.length; i++) {   	// find this inputs connections
-        if (connections[i]['target'].indexOf(id) !== -1) {  // found one connection that leads to this node (inputs have to be singeltons? Or can we have more than one input and we pick a random value?)
+        if (connections[i]['target'].indexOf(id) !== -1) {
+	    // found one connection that leads to this node (inputs have to be singeltons? Or can we have more than one input and we pick a random value?)
             // what is the node on the other end of the connection? This node is the target.
             var targetNodeID = connections[i]['source'];
             var targetNode = null;
@@ -266,6 +282,53 @@ function doneDone(recipe) {
 // cache all the worker objects to have local persistent worker memory
 var workers = {};
 
+function createWorker( id, state, node, recipe) {
+    switch (id ) {
+    case "redcap-measure-get":
+        return new RedcapGet(state);
+    case 'redcap-measure-put':
+        return new RedcapPut(node);
+    case 'not':
+        return new Not(recipe);
+    case 'filter':
+        return new Filter(recipe);
+    case 'mean':
+        return new Mean(recipe);
+    case 'equal':
+        return new Equal(recipe);
+    case 'smaller-equal':
+        return new SmallerEqual(recipe);
+    case 'greater-equal':
+        return new GreaterEqual(recipe);
+    case 'maximum':
+        return new Maximum(recipe);
+    case 'minimum':
+        return new Minimum(recipe);
+    case 'console-out':
+        return new ConsoleOut(recipe);
+    case 'if-else':
+        return new IfElse(recipe);
+    case 'all-not-empty':
+        return new AllNotEmpty(recipe);
+    case 'not-this-value':
+        return new NotThisValue(recipe);
+    case 'median':
+        return new Median(recipe);
+    case 'constant-number':
+        return new ConstantNumber(recipe);
+    case 'constant-text':
+        return new ConstantText(recipe);
+    case 't-score':
+        return new TScore(recipe);
+    case 'sum':
+        return new Sum(recipe);
+    default:
+        console.log("unknown module type: " + id);
+	return null;
+    }
+}
+
+
 // one tick for one node
 // returns if the current node is done or if there is more it could do in the next iteration (generator nodes)
 function work(node, recipe) {
@@ -275,12 +338,10 @@ function work(node, recipe) {
     var enabled = getEnabledValue(node, recipe);
     if (!enabled) {
         // if we are not enabled we should not have values in our output (state is special because we need values in there)
-        if (typeof node['outputs'] !== 'undefined')
+        if (typeof node['outputs'] !== 'undefined') {
             for (var i = 0; i < node['outputs'].length; i++)
                 delete node['outputs'][i]['value'];
-        /*if (typeof node['state'] !== 'undefined')
-           for (var i = 0; i < node['state'].length; i++)
-                delete node['state'][i]['value']; */
+        }
 
         return true; // don't do anything, this node is not enabled
     }
@@ -291,52 +352,25 @@ function work(node, recipe) {
     // collect the list of outputs
     var worker = null;
     if (typeof workers[gid] == 'undefined') { // create a worker for this node
-        if (node['id'] == "redcap-measure-get") {
-            worker = new RedcapGet(state); 
-        } else if (node['id'] == 'redcap-measure-put') {
-            worker = new RedcapPut(node);
-        } else if (node['id'] == 'not') {
-            worker = new Not(recipe);
-        } else if (node['id'] == 'filter') {
-            worker = new Filter(recipe);
-        } else if (node['id'] == 'mean') {
-            worker = new Mean(recipe);
-        } else if (node['id'] == 'equal') {
-            worker = new Equal(recipe);
-        } else if (node['id'] == 'smaller-equal') {
-            worker = new SmallerEqual(recipe);
-        } else if (node['id'] == 'greater-equal') {
-            worker = new GreaterEqual(recipe);
-        } else if (node['id'] == 'maximum') {
-            worker = new Maximum(recipe);
-        } else if (node['id'] == 'minimum') {
-            worker = new Minimum(recipe);
-        } else if (node['id'] == 'console-out') {
-            worker = new ConsoleOut(recipe);
-        } else if (node['id'] == 'if-else') {
-            worker = new IfElse(recipe);
-        } else if (node['id'] == 'all-not-empty') {
-            worker = new AllNotEmpty(recipe);
-        } else if (node['id'] == 'not-this-value') {
-            worker = new NotThisValue(recipe);
-        } else if (node['id'] == 'median') {
-            worker = new Median(recipe);
-        } else if (node['id'] == 'constant-number') {
-            worker = new ConstantNumber(recipe);
-        } else if (node['id'] == 'constant-text') {
-            worker = new ConstantText(recipe);
-        } else if (node['id'] == 'sum') {
-            worker = new Sum(recipe);
-        } else {
-            console.log("unknown module type: " + node['id']);
-        }
-        workers[gid] = { 'worker': worker, 'inputs': inputs, 'outputs': outputs, 'state': state }; // we want to check if any of the inputs or outputs or the internal state changed, if they did not change we are done
+	worker = createWorker( node['id'], state, node, recipe );
+	// we want to check if any of the inputs or outputs or the internal state changed, if they did not change we are done
+	if (worker !== null)
+            workers[gid] = { 'worker': worker, 'inputs': inputs, 'outputs': outputs, 'state': state, 'id': node['id'] }; 
     }
     worker = workers[gid]['worker'];
 
     var done = true;
     if (worker !== null) {
         worker.work(inputs, outputs, state);
+        if (historyFile != "") {
+            fs.appendFileSync(historyFile, JSON.stringify({ 
+                'node-id': node['id'],
+                'node-gid': gid,
+                'epoch': epoch, 
+                'inputs': inputs, 
+                'outputs': outputs }) + "\n" );
+        }
+
         // is the state different? or the outputs? or the inputs?
         if (isEquivalent(workers[gid]['inputs'], inputs) && 
             isEquivalent(workers[gid]['outputs'], outputs) && 
@@ -350,24 +384,8 @@ function work(node, recipe) {
         workers[gid]['outputs'] = clone(outputs);
         workers[gid]['state']   = clone(state);
 
-//        if (typeof worker.epoch !== 'undefined') {
-//            var doneDone = worker.epoch(epoch);
-//            // do we know if an epoch is empty?
-//            if (doneDone) { // there is no more work to do, stop here
-//                epoch = -1; // indicate that there are no more epochs
-//            }
-//        }
-
-        // what work is producing in outputs is an object with variable names that represent state variables values
-        // these values need to be assigned to the output ports that correspond to the state variables
-
-//        if (done && typeof worker.done !== 'undefined') {
-//            done = done && worker.done();
-//        }
-        // ok, now we have the outputs from this module, what do we do with those?
-        // set the outputs values to the output ports of this module as values
-
-        // we should set all other values for the outputs to undefined - if we only get a small number of values we should not have old values in the output ports of this node
+        // we should set all other values for the outputs to undefined -
+	// if we only get a small number of values we should not have old values in the output ports of this node
         for (var i = 0; i < node['outputs'].length; i++) {
             delete node['outputs'][i]['value'];
         }
@@ -383,20 +401,28 @@ function work(node, recipe) {
                         break;
                     }
                 }
-            } //else {
-                // as an alternative we don't have an internal state, in that case we just copy the values into the output by name
-                for (var j = 0; j < node['outputs'].length; j++) {
-                    if (node['outputs'][j]['name'] == outValNames[i]) {
-                        node['outputs'][j]['value'] = outputs[outValNames[i]];
-                        break;
-                    }
+            }
+            // as an alternative we don't have an internal state, in that case we just copy the values into the output by name
+            for (var j = 0; j < node['outputs'].length; j++) {
+                if (node['outputs'][j]['name'] == outValNames[i]) {
+                    node['outputs'][j]['value'] = outputs[outValNames[i]];
+                    break;
                 }
-            //}
+            }
         }
     }
     return done;
 }
 
+// this will not work because workers cannot be stringified
+function saveEnvironment ( filename, recipe ) {
+    var all = ['epoch', 'recipe', 'workers' ];
+    var storageObject = {};
+    for(var i = 0; i < all.length; i++) {
+	storageObject[all[i]] = eval(all[i]);
+    }
+    fs.writeFileSync(filename, JSON.stringify(storageObject), 'utf-8');
+}
 
 var epoch = 0; // we have to wait for the graph to finish processing before we can use the next generated data item and start over
 function run(recipe) {
@@ -412,26 +438,12 @@ function run(recipe) {
     if ( !readyForEpoch(recipe) ) { // only if we are ready for this epoch look at somethingChanged to find out if we are done
         somethingChanged = true; 
     }
-    if (epoch == 0) { // is this too early?
-        //startEpoch(recipe);
-    }
 
     if ( doneDone(recipe) ) { // everyone is done
-//      if (epoch == -1) {
         // no more data, stop here
         console.log("DONE DONE, epochs ended...");
         // lets ask each module to cleanUp
         cleanUp(recipe);
-/*        for (var i = 0; i < recipe['nodes'].length; i++) {
-            var node = recipe['nodes'][i];
-            var gid = node['gid'];
-            if (typeof workers[gid] !== 'undefined') {
-                var worker = workers[gid]['worker'];
-                if (typeof worker.cleanUp !== 'undefined') {
-                    worker.cleanUp();
-                }
-            }
-        } */
 
         return;
     }
@@ -440,18 +452,79 @@ function run(recipe) {
         epoch = epoch + 1; // and try again with the next participant
         startEpoch(recipe); // notify all nodes that a new epoch should start - wait with readyForEpoch to find out if we are ready to process
     }
-    setTimeout(function () {
+    setTimeout(function() {
+	//console.log("DEBUGFILE: " + debugFile + " numsteps: " + numSteps + "\n");
+	if (debugFile !== "" && numSteps >= 0) {
+	    numSteps = numSteps - 1;
+	}
+	if (debugFile !== "" && numSteps < 0) {
+	    // we are done, save the environment for the next iteration in debugFile
+	    saveEnvironment(debugFile, recipe);
+	    return;
+	}
         run(recipe);
     }, 10); // wait minimum time period -- 4ms?, in the meantime nodes have time to pull values from REDCap
-
+    
 }
 
 let runSetup = (file, options) => {
     let params = [];
-    if (options.output)
-        params.push('o')
-    if (options.input)
-        params.push('i')
+    if (options.history)
+        params.push('h')
+    if (options.debug)
+        params.push('d')
+    if (options.steps)
+        params.push('s')
+
+    if (options.history && options.history != "") {
+        console.log("Info: will write history information to " + options.historyFile);
+        historyFile = options.debug;
+    }
+
+    if (options.steps && options.steps != "") {
+        console.log("Info: will perform " + options.steps + " program steps");
+        numSteps = options.steps;
+    }
+
+    if (options.debug && options.debug != "") {
+        console.log("Info: will store debugging information in " + options.debug);
+        debugFile = options.debug;
+	// if we have this file already try to continue using the information inside
+	if (fs.existsSync(debugFile)) {
+	    // read and get values from it
+	    var content = JSON.parse(fs.readFileSync(debugFile, 'utf8'));
+	    // distribute the results to the internal variables
+	    epoch    = content['epoch'];
+	    recipe   = content['recipe'];
+	    workers  = content['workers'];
+	    var keys = Object.keys(workers);
+	    for (var i = 0; i < keys.length; i++) {
+		// find the node
+		var node = null;
+		for(var j = 0; j < recipe['nodes'].length; j++) {
+		    if (recipe['nodes'][j]['id'] == workers[keys[i]]['id']) {
+			node = recipe['nodes'][j];
+			break;
+		    }
+		}
+		if (node === null) {
+		    console.log("Error: unknown node in debugFile");
+		}
+		var state  = (typeof node['state'] === 'undefined') ? {} : node['state'];
+		var worker = createWorker(workers[keys[i]]['id'], state, node, recipe);
+		// after we create a worker we need to copy the existing internal variables over
+		var objs = Object.keys(workers[keys[i]]['worker']);
+		for (var k = 0; k < objs.length; k++) {
+		    worker[objs[k]] = workers[keys[i]]['worker'][objs[k]];
+		}				      
+		workers[keys[i]]['worker'] = worker;
+	    }
+	    // start the processing
+	    run(recipe);
+	    return;
+	}
+	
+    }
 
     if (file == "" || typeof file === 'undefined') {
         console.log("Error: please provide a filename");
@@ -473,8 +546,10 @@ let runSetup = (file, options) => {
 program
     .version('0.0.1')
     .command('run [file]')
-    .option('-o, --output', 'output file name')
     .option('-i, --input', 'input file name')
+    .option('-h, --history [historyFile]', 'history file name')
+    .option('-d, --debug [debugFile]', 'debug file name')
+    .option('-s, --steps [numSteps]', 'number of steps to perform')
     .action(runSetup);
 
 program.parse(process.argv); // end with parse to parse through the input
