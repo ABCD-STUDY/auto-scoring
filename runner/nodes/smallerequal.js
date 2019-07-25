8308bf0671ec32c5f54ce3584d63d26e46aae741
Hauke Bartsch <HaukeBartsch@gmail.com>
2019-07-25 05:36:30 -0700

    use moment to calculate dates

--- runner/nodes/smallerequal.js
+++ runner/nodes/smallerequal.js
@@ -2,17 +2,50 @@
 // calculate SmallerEqual
 //
 
+var moment = require('moment');
+
 var SmallerEqual = function (env) {
     // create this function only once, otherwise we think this nodes output changed
     this._condition = function(a,b) {
-	var toString = Object.prototype.toString;
-	if ( toString.call(a) == '[object String]' ) {
-	    a = parseFloat(a);
-	}
-	if ( toString.call(b) == '[object String]' ) {
-	    b = parseFloat(b);
-	}
-	return a <= b;
+        var aAsDate = null;
+        var bAsDate = null;        
+        
+	    var toString = Object.prototype.toString;
+	    if ( toString.call(a) == '[object String]' ) {
+            if (moment(a, "MM-DD-YYYY", true).isValid()) {
+                aAsDate = moment(a, "MM-DD-YYYY", true);
+                console.log("parsed " + a + " as a date " + aAsDate.format());
+            }
+            if (moment(a, 'YYYY-MM-DD', true).isValid()) {
+                aAsDate = moment(a, 'YYYY-MM-DD', true);
+                console.log("parsed " + a + " as a date " + aAsDate.format());
+            }
+            if (moment(a, 'YYYY-MM-DD HH:mm', true).isValid()) {
+                aAsDate = moment(a, 'YYYY-MM-DD HH:mm', true);
+                console.log("parsed " + a + " as a date " + aAsDate.format());
+            }
+	        a = parseFloat(a);
+	    }
+	    if ( toString.call(b) == '[object String]' ) {
+            if (moment(b, "MM-DD-YYYY", true).isValid()) {
+                bAsDate = moment(b, "MM-DD-YYYY", true);
+                console.log("parsed " + b + " as a date " + bAsDate.format());
+            }
+            if (moment(b, 'YYYY-MM-DD', true).isValid()) {
+                bAsDate = moment(b, 'YYYY-MM-DD', true);
+                console.log("parsed " + b + " as a date " + bAsDate.format());
+            }
+            if (moment(b, 'YYYY-MM-DD HH:mm', true).isValid()) {
+                bAsDate = moment(b, 'YYYY-MM-DD HH:mm', true);
+                console.log("parsed " + b + " as a date " + bAsDate.format());
+            }
+	        b = parseFloat(b);
+	    }
+        if (aAsDate !== null && bAsDate !== null) {
+            return aAsDate.isSameOrBefore(bAsDate);
+        }
+        
+	    return a <= b;
     };
 };
 
